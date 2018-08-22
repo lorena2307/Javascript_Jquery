9c47cbefe0b3f8a8aaa2789b8b169e5ed21bd3c0
Lorena Zarate Vera <anerol_brumars@hotmail.com>
2018-08-22 13:33:36 -0500

    Practicas_JavaS_Jquery

---
+++ practica2/js/codigo.js
@@ -0,0 +1,9 @@
+//en esta parte de js se empleo la funcion de alert para que esta nos mande una queña ventana en donde nos mande aviso o mensaje,
+//se muestra un primer mensaje, y despues mostrara otro mensaje que diga "Soy el primer scritp"
+alert("Practica de Inclusion");
+alert("Soy el primer Script");
+//en pantalla despues de mostrar los mensajes nos mostrara Practicas Inclusion 2
+function Saludar(nombre){
+  document.write(`Practica${nombre}`);
+}
+Saludar(" Inclusion 2 ")
---
+++ practica2/js/ejemplo.js
@@ -0,0 +1,23 @@
+alert("Operacion Suma de 100+80 es: ");
+  function Suma(Num1,Num2){
+    return Num1+Num2
+  }
+  alert(Suma(100,80))
+
+  alert("Operacion Resta de 20-8 es: ");
+    function Resta(Num1,Num2){
+      return Num1-Num2
+    }
+    alert(Resta(20,8))
+
+    alert("Operacion multiplicacion de 5*8 es: ");
+      function Multiplicacion(Num1,Num2){
+        return Num1*Num2
+      }
+      alert(Multiplicacion(5,8))
+
+      alert("Operacion Divición de 40/3 es: ");
+        function Division(Num1,Num2){
+          return Num1/Num2
+        }
+        alert(Division(40,3))
